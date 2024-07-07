c56dc09ae2aa07c9be3679a928da195e71d2bf5e
hau-storyline <bichhau1213@gmail.com>
2024-07-08 03:03:14 +0700

    international etiquette

---
+++ story_content/triggers.js
@@ -0,0 +1,61 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5yj9yAfgUEB":
+        Script1();
+        break;
+      case "5aYLq1YFLr8":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5VawEfl8EnN');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6n4OQMuOAKt';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5VawEfl8EnN');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6n4OQMuOAKt';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
